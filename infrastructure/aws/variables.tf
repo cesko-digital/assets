350b1ec7022f110488590b671bf77a8cdd736966
Martin Wenisch <martin.wenisch@cesko.digital>
2022-04-25 11:44:20 +0200

    Refactored Terraform and updated deprecated features.

---
+++ infrastructure/aws/variables.tf
@@ -0,0 +1,19 @@
+variable "aws-region" {
+  type = string
+}
+
+variable domain {
+  type = string
+}
+
+variable "ssl_certificate_arn" {
+  type = string
+}
+
+variable "manual-bucket-name" {
+  type = string
+}
+
+variable "automated-bucket-name" {
+  type = string
+}
